4a664429ba93281c4bcfa00b3a915e8783648286
Reed Barber <reed.barber@gmail.com>
2024-03-09 08:53:50 -0800

    Update global-data.js 2

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'lonoak';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'lonoak.com';
+    : 'Experience Tranquility: Lonoak Central Coast's Best Kept Secret';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
